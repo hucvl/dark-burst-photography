cd9aa413e3f510b9a2e6497b9d9883cd1e3aa2f0
Ahmet Serdar Karadeniz <ahmetserdarkaradeniz@rayinformatics.com>
2020-06-18 11:56:49 +0300

    Some code cleanup.

--- src/burst_nets.py
+++ src/burst_nets.py
@@ -60,10 +60,6 @@ def encode_block(inputs, dims, activation_fn,  block_idx, max_pool=True,  module
 
     conv = tf.map_fn(lambda x: slim.conv2d(x,dims,[ksize1,ksize1], rate=1, activation_fn=activation_fn,scope='g_conv%d_1%s'%(block_idx, module_name), reuse=tf.AUTO_REUSE), inputs)
     encs = tf.map_fn(lambda x: slim.conv2d(x,dims,[ksize2,ksize2], rate=1, activation_fn=activation_fn,scope='g_conv%d_2%s'%(block_idx, module_name), reuse=tf.AUTO_REUSE), conv)  
-    
-    ## se block.
-    # if "fine" in module_name:
-    #     encs = tf.map_fn(lambda x: se_block(x, dims), encs)
 
     global_pool = tf.reduce_max(encs, 0)
 
@@ -86,32 +82,6 @@ def decode_block(inputs, inputs_early, out_channels, in_channels, activation_fn,
     global_pool = tf.reduce_max(conv, 0)
 
     return conv, global_pool
-
-def se_block(x, dims,  scope,  ratio=16):
-    squeeze = tf.reduce_mean(x, axis=[1,2]) ## global avg pool.
-    excitation = slim.fully_connected(squeeze, dims//ratio, scope=scope)
-    excitation = tf.nn.relu(excitation)
-    excitation = slim.fully_connected(excitation, dims, scope=scope)
-    excitation = tf.sigmoid(excitation)
-    excitation = tf.reshape(excitation, [-1,1,1,dims])
-
-    out = x*excitation
-    return out
-
-# def ca_block(x, dims, scope, ratio=16):
-#     kernel_initializer = tf.contrib.layers.variance_scaling_initializer()
-#     bias_initializer = tf.constant_initializer(value=0.0)
-
-#     squeeze = tf.reduce_mean(x, axis=[1,2], keepdims=True) ## global avg pool.
-#     excitation = slim.fully_connected(squeeze, dims//ratio, scope="%s_1"%scope,weights_initializer=kernel_initializer, biases_initializer=bias_initializer)
-#     excitation = tf.nn.relu(excitation)
-#     excitation = slim.fully_connected(excitation, dims, scope="%s_2"%scope, weights_initializer=kernel_initializer, biases_initializer=bias_initializer)
-#     excitation = tf.sigmoid(excitation)
-#     #excitation = tf.reshape(excitation, [-1,1,1,dims])
-
-#     out = x*excitation
-#     return out
-
 
 ## Coarse module.
 def coarse_net(inputs, dims=32, module_name="original"):
@@ -137,7 +107,6 @@ def coarse_net(inputs, dims=32, module_name="original"):
 ## Fine module.
 def fine_net(inputs, coarse_outs, dims=32, module_name="fine"):
     coarse_outs = tf.map_fn(lambda x: utils.tf_upsample(x), coarse_outs)
-    #res = tf.abs(inputs - coarse_outs) + 0.5
     res =  inputs - coarse_outs
     inputs_ = tf.concat([inputs, res, coarse_outs], axis=4)
 
@@ -150,124 +119,21 @@ def fine_net(inputs, coarse_outs, dims=32, module_name="fine"):
     up6 = upsample_and_concat(conv5, conv4, dims*8, dims*16, is_fine=True, block_idx=0)
     conv6 = slim.conv2d(up6, dims*8, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv6_1_%s'%module_name,  reuse=tf.AUTO_REUSE)
     conv6 = slim.conv2d(conv6, dims*8, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv6_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv6 = se_block(conv6,dims*8)
+
 
     up7 = upsample_and_concat(conv6, conv3, dims*4, dims*8, is_fine=True, block_idx=1)
     conv7 = slim.conv2d(up7, dims*4, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv7_1_%s'%module_name, reuse=tf.AUTO_REUSE)
     conv7 = slim.conv2d(conv7, dims*4, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv7_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv7 = se_block(conv7,dims*4)
 
     up8 = upsample_and_concat(conv7, conv2, dims*2, dims*4, is_fine=True, block_idx=2)
     conv8 = slim.conv2d(up8, dims*2, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv8_1_%s'%module_name, reuse=tf.AUTO_REUSE)
     conv8 = slim.conv2d(conv8, dims*2, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv8_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv8 = se_block(conv8,dims*2)
 
     up9 = upsample_and_concat(conv8, conv1, dims, dims*2, is_fine=True, block_idx=3)
     conv9 = slim.conv2d(up9, dims, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv9_1_%s'%module_name, reuse=tf.AUTO_REUSE)
     conv9 = slim.conv2d(conv9, dims, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv9_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv9 = se_block(conv9,dims,ratio=8)
 
     conv10 = slim.conv2d(conv9, 12, [1, 1], rate=1, activation_fn=None, scope='g_conv10_%s'%module_name, reuse=tf.AUTO_REUSE)
     out = tf.depth_to_space(conv10, 2)
 
-    #decoder_layers.append(conv10s)
-    return out
-
-## Fine module with residual blocks.
-def fine_res_net(inputs, coarse_outs, dims=32, module_name="fine"):
-    coarse_outs = tf.map_fn(lambda x: utils.tf_upsample(x), coarse_outs)
-    #res = tf.abs(inputs - coarse_outs) + 0.5
-    res =  inputs - coarse_outs
-    inputs_ = tf.concat([inputs, res, coarse_outs], axis=4)
-
-    pool1s, conv1s, conv1 = encode_block(inputs_, dims,  activation_fn=lrelu, module_name=module_name, block_idx=1)
-    pool2s, conv2s, conv2 = encode_block(pool1s, dims*2, activation_fn=lrelu, module_name=module_name, block_idx=2)
-    pool3s, conv3s, conv3 = encode_block(pool2s, dims*4, activation_fn=lrelu, module_name=module_name, block_idx=3)
-    pool4s, conv4s, conv4 = encode_block(pool3s, dims*8, activation_fn=lrelu, module_name=module_name, block_idx=4)
-    conv5s, conv5s, conv5 = encode_block(pool4s, dims*16, activation_fn=lrelu, module_name=module_name, block_idx=5, max_pool=False)
-
-    net = conv5
-    for i in range(4):
-        temp = net
-        net = slim.conv2d(net, dims*16, [3,3], activation_fn=lrelu, normalizer_fn=layer_norm, scope='g_conv_%s_res_%d_1' % (module_name, i))
-        net = slim.conv2d(net, dims*16, [3,3], activation_fn=None, normalizer_fn=layer_norm, scope='g_conv_%s_res_%d_2' % (module_name, i))
-        net = net + temp
-      
-    net = slim.conv2d(net, dims*16, [3,3], activation_fn=None,normalizer_fn=layer_norm, scope='g_conv_%s_res'%module_name)
-    conv5 = net + conv5
-
-    up6 = upsample_and_concat(conv5, conv4, dims*8, dims*16, is_fine=True, block_idx=0)
-    conv6 = slim.conv2d(up6, dims*8, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv6_1_%s'%module_name,  reuse=tf.AUTO_REUSE)
-    conv6 = slim.conv2d(conv6, dims*8, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv6_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv6 = se_block(conv6,dims*8)
-
-    up7 = upsample_and_concat(conv6, conv3, dims*4, dims*8, is_fine=True, block_idx=1)
-    conv7 = slim.conv2d(up7, dims*4, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv7_1_%s'%module_name, reuse=tf.AUTO_REUSE)
-    conv7 = slim.conv2d(conv7, dims*4, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv7_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv7 = se_block(conv7,dims*4)
-
-    up8 = upsample_and_concat(conv7, conv2, dims*2, dims*4, is_fine=True, block_idx=2)
-    conv8 = slim.conv2d(up8, dims*2, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv8_1_%s'%module_name, reuse=tf.AUTO_REUSE)
-    conv8 = slim.conv2d(conv8, dims*2, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv8_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv8 = se_block(conv8,dims*2)
-
-    up9 = upsample_and_concat(conv8, conv1, dims, dims*2, is_fine=True, block_idx=3)
-    conv9 = slim.conv2d(up9, dims, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv9_1_%s'%module_name, reuse=tf.AUTO_REUSE)
-    conv9 = slim.conv2d(conv9, dims, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv9_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv9 = se_block(conv9,dims,ratio=8)
-
-    conv10 = slim.conv2d(conv9, 12, [1, 1], rate=1, activation_fn=None, scope='g_conv10_%s'%module_name, reuse=tf.AUTO_REUSE)
-    out = tf.depth_to_space(conv10, 2)
-
-    #decoder_layers.append(conv10s)
-    return out
-
-## Fine module with residual blocks.
-def fine_res_se_net(inputs, coarse_outs, dims=32, module_name="fine"):
-    coarse_outs = tf.map_fn(lambda x: utils.tf_upsample(x), coarse_outs)
-    #res = tf.abs(inputs - coarse_outs) + 0.5
-    res =  inputs - coarse_outs
-    inputs_ = tf.concat([inputs, res, coarse_outs], axis=4)
-
-    pool1s, conv1s, conv1 = encode_block(inputs_, dims,  activation_fn=lrelu, module_name=module_name, block_idx=1)
-    pool2s, conv2s, conv2 = encode_block(pool1s, dims*2, activation_fn=lrelu, module_name=module_name, block_idx=2)
-    pool3s, conv3s, conv3 = encode_block(pool2s, dims*4, activation_fn=lrelu, module_name=module_name, block_idx=3)
-    pool4s, conv4s, conv4 = encode_block(pool3s, dims*8, activation_fn=lrelu, module_name=module_name, block_idx=4)
-    conv5s, conv5s, conv5 = encode_block(pool4s, dims*16, activation_fn=lrelu, module_name=module_name, block_idx=5, max_pool=False)
-
-    net = conv5
-    for i in range(4):
-        temp = net
-        net = slim.conv2d(net, dims*16, [3,3], activation_fn=lrelu, normalizer_fn=layer_norm, scope='g_conv_%s_res_%d_1' % (module_name, i))
-        net = slim.conv2d(net, dims*16, [3,3], activation_fn=None, normalizer_fn=layer_norm, scope='g_conv_%s_res_%d_2' % (module_name, i))
-        net = net + temp 
-        net = se_block(net,  dims*16,  scope="g_conv_%s_res_se_%d"%(module_name,i), ratio=16)
-      
-    net = slim.conv2d(net, dims*16, [3,3], activation_fn=None,normalizer_fn=layer_norm, scope='g_conv_%s_res'%module_name)
-    conv5 = net + conv5
-
-    up6 = upsample_and_concat(conv5, conv4, dims*8, dims*16, is_fine=True, block_idx=0)
-    conv6 = slim.conv2d(up6, dims*8, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv6_1_%s'%module_name,  reuse=tf.AUTO_REUSE)
-    conv6 = slim.conv2d(conv6, dims*8, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv6_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv6 = se_block(conv6,dims*8)
-
-    up7 = upsample_and_concat(conv6, conv3, dims*4, dims*8, is_fine=True, block_idx=1)
-    conv7 = slim.conv2d(up7, dims*4, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv7_1_%s'%module_name, reuse=tf.AUTO_REUSE)
-    conv7 = slim.conv2d(conv7, dims*4, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv7_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv7 = se_block(conv7,dims*4)
-
-    up8 = upsample_and_concat(conv7, conv2, dims*2, dims*4, is_fine=True, block_idx=2)
-    conv8 = slim.conv2d(up8, dims*2, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv8_1_%s'%module_name, reuse=tf.AUTO_REUSE)
-    conv8 = slim.conv2d(conv8, dims*2, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv8_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv8 = se_block(conv8,dims*2)
-
-    up9 = upsample_and_concat(conv8, conv1, dims, dims*2, is_fine=True, block_idx=3)
-    conv9 = slim.conv2d(up9, dims, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv9_1_%s'%module_name, reuse=tf.AUTO_REUSE)
-    conv9 = slim.conv2d(conv9, dims, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv9_2_%s'%module_name, reuse=tf.AUTO_REUSE)
-    #conv9 = se_block(conv9,dims,ratio=8)
-
-    conv10 = slim.conv2d(conv9, 12, [1, 1], rate=1, activation_fn=None, scope='g_conv10_%s'%module_name, reuse=tf.AUTO_REUSE)
-    out = tf.depth_to_space(conv10, 2)
-
-    #decoder_layers.append(conv10s)
     return out
